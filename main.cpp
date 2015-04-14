b780455cc1477897adc268f3e8c309266077f757
Devath2008 <devath_56@hotmail.com>
2015-04-13 18:22:49 -0800

    Basic Starting Code

---
+++ main.cpp
@@ -0,0 +1,32 @@
+// Alexander Lease
+// L-Systems rendering simulation
+//
+// Created 04/13/2015
+#include <QCoreApplication>
+#inclide <vector>
+using std::string;
+using std::vector;
+using std::pair;
+typedef pair<int,int> coord;
+
+void pointSetter(vector<pair<coord,coord> > thePoints, string theString)
+{
+
+}
+
+string buildString(string start,string rules, int iterations)
+{
+
+    return start;
+}
+
+
+
+
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
+
+    return a.exec();
+}
